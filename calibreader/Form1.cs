eeb2cfa65f097a38e620187bc94857fb7c0b922b
jemicheng <jemicheng@hotmail.com>
2018-08-31 17:31:53 +0800

    implemeting export to excel done

--- calibreader/Form1.cs
+++ calibreader/Form1.cs
@@ -13,6 +13,7 @@ using System.IO.Compression;
 
 using ICSharpCode.SharpZipLib.BZip2;
 using ICSharpCode.SharpZipLib.Tar;
+using ClosedXML.Excel;
 
 namespace calibreader
 { 
@@ -97,6 +98,7 @@ namespace calibreader
             //Debug.WriteLine("1234");
             //this.viewcali.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
             this.viewcali.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.viewcali.AllowUserToAddRows = false;
         }
         /*private void viewcali_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -215,10 +217,84 @@ namespace calibreader
                 getStatsInFolder(folderDlg.SelectedPath);
             }
         }
+        public void ExportToExcelWithFormatting(DataGridView dataGridView1)
+        {
+            string fileName;
 
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "xls files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            saveFileDialog1.Title = "To Excel";
+            saveFileDialog1.FileName = this.Text + " (" + DateTime.Now.ToString("yyyy-MM-dd") + ")";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = saveFileDialog1.FileName;
+                var workbook = new XLWorkbook();
+                var worksheet = workbook.Worksheets.Add(this.Text);
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    worksheet.Cell(1, i + 1).Value = dataGridView1.Columns[i].Name;
+                }
+                Debug.WriteLine(dataGridView1.Rows.Count);
+                Debug.WriteLine(dataGridView1.Columns.Count);
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        Debug.WriteLine(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                        worksheet.Cell(i + 2, j + 1).Value = dataGridView1.Rows[i].Cells[j].Value.ToString();
+
+                        if (worksheet.Cell(i + 2, j + 1).Value.ToString().Length > 0)
+                        {
+                            XLAlignmentHorizontalValues align;
+
+                            switch (dataGridView1.Rows[i].Cells[j].Style.Alignment)
+                            {
+                                case DataGridViewContentAlignment.BottomRight:
+                                    align = XLAlignmentHorizontalValues.Right;
+                                    break;
+                                case DataGridViewContentAlignment.MiddleRight:
+                                    align = XLAlignmentHorizontalValues.Right;
+                                    break;
+                                case DataGridViewContentAlignment.TopRight:
+                                    align = XLAlignmentHorizontalValues.Right;
+                                    break;
+
+                                case DataGridViewContentAlignment.BottomCenter:
+                                    align = XLAlignmentHorizontalValues.Center;
+                                    break;
+                                case DataGridViewContentAlignment.MiddleCenter:
+                                    align = XLAlignmentHorizontalValues.Center;
+                                    break;
+                                case DataGridViewContentAlignment.TopCenter:
+                                    align = XLAlignmentHorizontalValues.Center;
+                                    break;
+
+                                default:
+                                    align = XLAlignmentHorizontalValues.Left;
+                                    break;
+                            }
+
+                            worksheet.Cell(i + 2, j + 1).Style.Alignment.Horizontal = align;
+
+                            XLColor xlColor = XLColor.FromColor(dataGridView1.Rows[i].Cells[j].Style.SelectionBackColor);
+                            worksheet.Cell(i + 2, j + 1).AddConditionalFormat().WhenLessThan(1).Fill.SetBackgroundColor(xlColor);
+
+                            worksheet.Cell(i + 2, j + 1).Style.Font.FontName = dataGridView1.Font.Name;
+                            worksheet.Cell(i + 2, j + 1).Style.Font.FontSize = dataGridView1.Font.Size;
+
+                        }
+                    }
+                }
+                worksheet.Columns().AdjustToContents();
+                workbook.SaveAs(fileName);
+                //MessageBox.Show("Done");
+            }
+        }
         private void exportFile(object sender, EventArgs e)
         {
-
+            ExportToExcelWithFormatting(this.viewcali);
         }
+       
     }
 }

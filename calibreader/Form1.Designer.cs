94f0f8e4eaecfbeafe5574b80ef6e8ad74fe76f0
jemicheng <jemicheng@hotmail.com>
2018-08-31 15:47:48 +0800

    open single file is done

--- calibreader/Form1.Designer.cs
+++ calibreader/Form1.Designer.cs
@@ -28,15 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
             this.viewcali = new System.Windows.Forms.DataGridView();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lens_type = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.focus_idx = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.zoom_idx = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.inf_tele_focus = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.inf_wide_focus = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.focus_dynamic_offset = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.show_log = new System.Windows.Forms.DataGridViewButtonColumn();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -44,6 +44,22 @@
             this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripTextBox4 = new System.Windows.Forms.ToolStripTextBox();
             this.toolStripTextBox5 = new System.Windows.Forms.ToolStripTextBox();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lens_type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.focus_idx = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zoom_idx = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.inf_tele_focus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.inf_wide_focus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.focus_dynamic_offset = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.focus_value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.icr_mode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.production = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.awb_low_adj = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.awb_high_info = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.awb_low_info = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lens_info = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cali_log = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productoin_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.viewcali)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -64,13 +80,74 @@
             this.inf_tele_focus,
             this.inf_wide_focus,
             this.focus_dynamic_offset,
-            this.show_log});
+            this.focus_value,
+            this.icr_mode,
+            this.production,
+            this.awb_low_adj,
+            this.awb_high_info,
+            this.awb_low_info,
+            this.lens_info,
+            this.cali_log,
+            this.productoin_time});
             this.viewcali.Location = new System.Drawing.Point(0, 27);
             this.viewcali.Name = "viewcali";
             this.viewcali.RowTemplate.Height = 24;
-            this.viewcali.Size = new System.Drawing.Size(887, 424);
+            this.viewcali.Size = new System.Drawing.Size(1338, 525);
             this.viewcali.TabIndex = 0;
-            this.viewcali.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.viewcali_CellContentClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem3});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1338, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem,
+            this.toolStripMenuItem2});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(39, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.openFolderToolStripMenuItem.Text = "Open File";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.onOpenFileClick);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(147, 22);
+            this.toolStripMenuItem2.Text = "Open Folder";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox4,
+            this.toolStripTextBox5});
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(47, 20);
+            this.toolStripMenuItem3.Text = "Help";
+            // 
+            // toolStripTextBox4
+            // 
+            this.toolStripTextBox4.Name = "toolStripTextBox4";
+            this.toolStripTextBox4.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox4.Text = "Check update";
+            // 
+            // toolStripTextBox5
+            // 
+            this.toolStripTextBox5.Name = "toolStripTextBox5";
+            this.toolStripTextBox5.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox5.Text = "About";
             // 
             // id
             // 
@@ -79,6 +156,8 @@
             // 
             // lens_type
             // 
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.lens_type.DefaultCellStyle = dataGridViewCellStyle1;
             this.lens_type.HeaderText = "lens_type";
             this.lens_type.Name = "lens_type";
             // 
@@ -107,72 +186,70 @@
             this.focus_dynamic_offset.HeaderText = "focus_dynamic_offset";
             this.focus_dynamic_offset.Name = "focus_dynamic_offset";
             // 
-            // show_log
+            // focus_value
             // 
-            this.show_log.HeaderText = "Show_log";
-            this.show_log.Name = "show_log";
-            this.show_log.Text = "Show log";
-            this.show_log.UseColumnTextForButtonValue = true;
+            this.focus_value.HeaderText = "focus_value";
+            this.focus_value.Name = "focus_value";
             // 
-            // menuStrip1
+            // icr_mode
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem1,
-            this.toolStripMenuItem3});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(887, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
+            this.icr_mode.HeaderText = "icr_mode";
+            this.icr_mode.Name = "icr_mode";
             // 
-            // toolStripMenuItem1
+            // production
             // 
-            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openFolderToolStripMenuItem,
-            this.toolStripMenuItem2});
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(39, 20);
-            this.toolStripMenuItem1.Text = "File";
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.production.DefaultCellStyle = dataGridViewCellStyle2;
+            this.production.HeaderText = "production";
+            this.production.Name = "production";
             // 
-            // openFolderToolStripMenuItem
+            // awb_low_adj
             // 
-            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
-            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.openFolderToolStripMenuItem.Text = "Open File";
-            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.onOpenFileClick);
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.awb_low_adj.DefaultCellStyle = dataGridViewCellStyle3;
+            this.awb_low_adj.HeaderText = "awb_low_adj";
+            this.awb_low_adj.Name = "awb_low_adj";
             // 
-            // toolStripMenuItem2
+            // awb_high_info
             // 
-            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
-            this.toolStripMenuItem2.Text = "Open Folder";
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.awb_high_info.DefaultCellStyle = dataGridViewCellStyle4;
+            this.awb_high_info.HeaderText = "awb_high_info";
+            this.awb_high_info.Name = "awb_high_info";
             // 
-            // toolStripMenuItem3
+            // awb_low_info
             // 
-            this.toolStripMenuItem3.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripTextBox4,
-            this.toolStripTextBox5});
-            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(47, 20);
-            this.toolStripMenuItem3.Text = "Help";
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.awb_low_info.DefaultCellStyle = dataGridViewCellStyle5;
+            this.awb_low_info.HeaderText = "awb_low_info";
+            this.awb_low_info.Name = "awb_low_info";
             // 
-            // toolStripTextBox4
+            // lens_info
             // 
-            this.toolStripTextBox4.Name = "toolStripTextBox4";
-            this.toolStripTextBox4.Size = new System.Drawing.Size(100, 23);
-            this.toolStripTextBox4.Text = "Check update";
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.lens_info.DefaultCellStyle = dataGridViewCellStyle6;
+            this.lens_info.HeaderText = "lens_info";
+            this.lens_info.Name = "lens_info";
             // 
-            // toolStripTextBox5
+            // cali_log
             // 
-            this.toolStripTextBox5.Name = "toolStripTextBox5";
-            this.toolStripTextBox5.Size = new System.Drawing.Size(100, 23);
-            this.toolStripTextBox5.Text = "About";
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.cali_log.DefaultCellStyle = dataGridViewCellStyle7;
+            this.cali_log.HeaderText = "cali_log";
+            this.cali_log.Name = "cali_log";
+            // 
+            // productoin_time
+            // 
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.productoin_time.DefaultCellStyle = dataGridViewCellStyle8;
+            this.productoin_time.HeaderText = "productoin_time";
+            this.productoin_time.Name = "productoin_time";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(887, 450);
+            this.ClientSize = new System.Drawing.Size(1338, 551);
             this.Controls.Add(this.viewcali);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -194,6 +271,8 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
         private System.Windows.Forms.ToolStripTextBox toolStripTextBox4;
         private System.Windows.Forms.ToolStripTextBox toolStripTextBox5;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
         private System.Windows.Forms.DataGridViewTextBoxColumn id;
         private System.Windows.Forms.DataGridViewTextBoxColumn lens_type;
         private System.Windows.Forms.DataGridViewTextBoxColumn focus_idx;
@@ -201,9 +280,15 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn inf_tele_focus;
         private System.Windows.Forms.DataGridViewTextBoxColumn inf_wide_focus;
         private System.Windows.Forms.DataGridViewTextBoxColumn focus_dynamic_offset;
-        private System.Windows.Forms.DataGridViewButtonColumn show_log;
-        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn focus_value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn icr_mode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn production;
+        private System.Windows.Forms.DataGridViewTextBoxColumn awb_low_adj;
+        private System.Windows.Forms.DataGridViewTextBoxColumn awb_high_info;
+        private System.Windows.Forms.DataGridViewTextBoxColumn awb_low_info;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lens_info;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cali_log;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productoin_time;
     }
 }
 
--- calibreader/Form1.cs
+++ calibreader/Form1.cs
@@ -18,6 +18,73 @@ namespace calibreader
 { 
     public partial class Form1 : Form
     {
+        public static bool ExtractTGZ(String gzArchiveName, String destFolder)
+        {
+            Stream inStream = File.OpenRead(gzArchiveName);
+            Stream gzipStream;
+            TarArchive tarArchive;
+            try
+            {
+                gzipStream = new BZip2InputStream(inStream);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+                return false; }
+
+            try
+            {
+                tarArchive = TarArchive.CreateInputTarArchive(gzipStream);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+                return false; }
+            tarArchive.ExtractContents(destFolder);
+            tarArchive.Close();
+
+            gzipStream.Close();
+            inStream.Close();
+            return true;
+        }
+        public static string getProductionLog(String filepath)
+        {
+            string result = "";
+            int lineCount = 1;
+
+            if (!File.Exists(filepath))
+                return "none";
+            else
+            {
+                string[] lines = File.ReadAllLines(filepath);
+                foreach (string stat in lines)
+                {
+                    if (!string.IsNullOrEmpty(stat))
+                    {
+
+                        if (lines.Length > 1)
+                        {
+                            result += stat;
+                            if (lineCount != lines.Length)
+                            {
+                                result += "\r\n";
+                                lineCount++;
+                            }
+
+                        }
+                        else
+                            result += stat;
+                    }
+                    else return "empty";
+
+                }
+            }
+            return result;
+        }
+        public static bool IsDirectoryEmpty(string path)
+        {
+            return !Directory.EnumerateFileSystemEntries(path).Any();
+        }
         public Form1()
         {
             InitializeComponent();
@@ -28,9 +95,10 @@ namespace calibreader
             viewcali.Rows.Add(row);*/
             //this.viewcali.Rows.Add("1", "2", "3", "4", "5", "6", "7");
             //Debug.WriteLine("1234");
-           
+            //this.viewcali.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.viewcali.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
         }
-        private void viewcali_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        /*private void viewcali_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             // Ignore clicks that are not in our 
             if (e.ColumnIndex == this.viewcali.Columns["show_log"].Index && e.RowIndex >= 0)
@@ -38,10 +106,47 @@ namespace calibreader
                 String str = this.viewcali.Rows[e.RowIndex].Cells[1].Value.ToString();
                 Debug.WriteLine("Button on row {0} clicked value {1}", e.RowIndex,str);
             }
+        }*/
+
+
+        public void getStats(string filePath,string directory_path)
+        {          
+            string tmppath = directory_path + "\\calitmp";
+            string statpath = directory_path + "\\calitmp\\mnt\\nand\\";
+
+            DirectoryInfo di;
+            if (Directory.Exists(tmppath))
+            {
+                if (!IsDirectoryEmpty(tmppath))
+                    Directory.Delete(tmppath, true);
+            }
+            di = Directory.CreateDirectory(tmppath);
+            if (File.Exists(filePath))
+            {
+                ExtractTGZ(filePath,tmppath);
+                string focus_idx = getProductionLog(statpath + "\\lens\\focus_idx");
+                string focus_dynamic_offset = getProductionLog(statpath + "\\lens\\focus_dynamic_offset");               
+                string inf_tele_focus = getProductionLog(statpath + "\\lens\\inf_tele_focus");                
+                string inf_wide_focus = getProductionLog(statpath + "\\lens\\inf_wide_focus");                
+                string lens_type = getProductionLog(statpath + "\\lens\\lens_type");               
+                string zoom_idx = getProductionLog(statpath + "\\lens\\zoom_idx");               
+                string focus_value = getProductionLog(statpath + "\\lens\\focus_value");               
+                string icr_mode = getProductionLog(statpath + "\\lens\\icr_mode");               
+                string production = getProductionLog(statpath + "\\factory\\production");              
+                string production_time = getProductionLog(statpath + "\\factory\\production_time");              
+                string testcase_time = getProductionLog(statpath + "\\factory\\TestCase_time");             
+                string awb_low_adj = getProductionLog(statpath + "\\cam_cali\\awb_low_adj");             
+                string cali_log = getProductionLog(statpath + "\\cam_cali\\cali_log");               
+                string awb_high_info = getProductionLog(statpath + "\\cam_cali\\debug\\awb_high_info");              
+                string awb_low_info = getProductionLog(statpath + "\\cam_cali\\debug\\awb_low_info");               
+                string lens_info = getProductionLog(statpath + "\\cam_cali\\debug\\lens_info");
+                string[] pathSplit = filePath.Split('\\');
+                string[] fileName = pathSplit.Last().Split('.');
+                this.viewcali.Rows.Add(fileName[0].ToString(), lens_type, focus_idx,zoom_idx,inf_tele_focus,inf_wide_focus,focus_dynamic_offset,focus_value,icr_mode,production,awb_low_adj,awb_high_info,awb_low_info,lens_info,cali_log,production_time);
+            }
+            di.Delete(true);
         }
-
-
-
+        
 
         private void onOpenFileClick(object sender, EventArgs e)
         {
@@ -51,8 +156,11 @@ namespace calibreader
             openFileDialog1.Title = "Select a camera log File";
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                Debug.WriteLine(openFileDialog1.FileName);
+                //Debug.WriteLine(openFileDialog1.FileName);
+                //Debug.WriteLine(Path.GetDirectoryName(openFileDialog1.FileName));
+                getStats(openFileDialog1.FileName, Path.GetDirectoryName(openFileDialog1.FileName));
             }
         }
+
     }
 }
